e936bf71f246349403d465ef1c64a77ce2ebcf06
Svyatoslav <slavanosov@yandex.ru>
2017-12-13 01:48:12 +0300

    Add files
    Add no golang files: css, js, html, yaml.

---
+++ dev/assets/js/script.js
@@ -0,0 +1,8 @@
+function formatSize(length){
+    var i = 0, type = ['б','Кб','Мб','Гб','Тб','Пб'];
+    while((length / 1000 | 0) && i < type.length - 1) {
+        length /= 1024;
+        i++;
+    }
+    document.getElementById('div_for_insert').innerHTML = length.toFixed(2) + ' ' + type[i];
+}
